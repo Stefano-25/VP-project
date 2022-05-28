dc429cf0d16e40405ec50798c20c9e3f1139a40d
Paolo Ferretti <104980356+paolofrt99@users.noreply.github.com>
2022-05-28 14:29:17 +0200

    Added loop through the video and face detection

--- detection_optflow.py
+++ detection_optflow.py
@@ -7,56 +7,72 @@ import os
 import random
 
 
+## Number of gpus available
+#ngpu = 1
+#device = torch.device('cuda:0' if (
+    #torch.cuda.is_available() and ngpu > 0) else 'cpu')
 def main():
-    
-    ## Number of gpus available
-    #ngpu = 1
-    #device = torch.device('cuda:0' if (
-        #torch.cuda.is_available() and ngpu > 0) else 'cpu')
 
-    # Take random file from the data folder
+    # Generate random number to extract a random video from the folder deepfakes
     base_dir = r"C:\Users\paolo\OneDrive\Desktop\project_OF\data\original_sequences\youtube\c23\videos"
     file_name = random.choice(os.listdir(base_dir))
     path_to_video = os.path.join(base_dir, file_name)
 
-    # Extract the video inside cap and read the first two frames.
-    # Extract the video and the first frame, convert to gray scale.
+    # Load the cascade
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    
+    # Extract the video inside cap and read the first two frames
     cap = cv2.VideoCapture(path_to_video)
     ret, old_frame = cap.read()
     frame1_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
-    
-    # Creates an array filled with zero 
-    # with the same dimensions of the frame
     hsv = np.zeros_like(old_frame)
     hsv[..., 1] = 255
 
-    # read second frame and convert to gray scale
-    ret, new_frame = cap.read()
-    if not ret:
+    while(1):
+        
+        if not ret:
             print('No frames grabbed!')
-            quit()
-    frame2_gray = cv2.cvtColor(new_frame, cv2.COLOR_BGR2GRAY)
-
-    ## optical flow with Farneback method
-    flow = cv2.calcOpticalFlowFarneback(frame1_gray, frame2_gray, None, 0.5, 3, 3, 1, 5, 1.2, 0)
-    
-    # magnitude and angle of the 2D vectors
-    mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
-
-    # Sets image hue and value according to the optical flow direction
-    # and magnitude, then converts HSV to RGB (BGR) color representation
-    hsv[..., 0] = ang*180/np.pi/2
-    hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
-    bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-    
-    ## Display frames and optical flow
-    # cv2.imshow('optical flow', hsv)
-    cv2.imshow('optical flow', bgr)
-    cv2.imshow('frame1', frame1_gray)
-    cv2.imshow('frame2', frame2_gray)
-    cv2.waitKey()
+            break
+        
+        # Detect the faces
+        faces = face_cascade.detectMultiScale(frame1_gray, 1.6, 4)
+        
+        # Draw the rectangle around each face
+        for (x, y, w, h) in faces:
+            cv2.rectangle(old_frame, (x, y), (x+w, y+h), (255, 0, 0), 4)
+        
+        # Read new frame
+        ret, new_frame = cap.read()
+        frame2_gray = cv2.cvtColor(new_frame, cv2.COLOR_BGR2GRAY)
+        
+        # Optical flow with Farneback method
+        flow = cv2.calcOpticalFlowFarneback(frame1_gray, frame2_gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+        
+        # Magnitude and angle of the 2D vectors
+        mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+        
+        # Sets image hue and value according to the optical flow direction
+        # and magnitude, then converts HSV to RGB (BGR) color representation
+        hsv[..., 0] = ang*180/np.pi/2
+        hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
+        bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+        
+        # Display frames and optical flow
+        cv2.imshow('optical flow bgr', bgr)
+        cv2.imshow('img', old_frame)
+        # cv2.imshow('optical flow hsv', hsv)
+        old_frame = new_frame
+        frame1_gray = frame2_gray
+        
+        # Frames are read by intervals of 1 millisecond. The
+        # programs breaks out of the while loop when the
+        # user presses the 'q' key
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        
     cap.release()
     cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     main()
