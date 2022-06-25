b3610657e00b4c8b50549c9c3644037ce93e5eb7
Stefano <servillostefano@gmail.com>
2022-06-25 18:41:34 +0200

    Final model

--- dataset/config.py
+++
@@ -1,9 +0,0 @@
-# specify path to the flowers and mnist dataset
-DATASET_PATH = "dataset/data"
-
-# specify the paths to our training and validation set 
-TRAIN = "dataset/train"
-VALIDATION = "dataset/val"
-
-# set validation data split
-VAL_SPLIT = 0.1
